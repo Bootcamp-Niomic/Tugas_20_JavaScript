215fd08e3e80252090180fde1c049912b9288315
Havid <great_havid@yahoo.co.id>
2019-11-13 11:44:54 +0700

    Tugas 20 JS

---
+++ tugas20.js
@@ -0,0 +1,11 @@
+// ====== Tugas 20 JavaScript =====
+
+function init() {
+    var data = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+    var data2 = data.match("are");
+    var data3 = data.match("words");
+    console.log(data2)
+    console.log(null);
+    console.log(data3);
+  }
+  init()
